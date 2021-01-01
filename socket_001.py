43a1b7046ca2600baa69ec3525a066bfdc6fd860
Алексей Куликов <alex@MacBook-Pro-Aleksej.local>
2021-01-01 22:12:07 +0300

    commit_001_01.01.2021

--- socket_001.py
+++ socket_001.py
@@ -1,16 +1,31 @@
 import socket
+from termcolor import colored
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
 
-print('connected:', addr)
 
-while True:
-    data = conn.recv(1024)
-    if not data:
-        break
-    conn.send(data.upper())
+"""функция получения данных от домашнего сервера"""
+def receive_data_from_server():
+    """подключение сокета"""
+    sock = socket.socket()
+    sock.bind(('', 9090))
+    sock.listen(1)
+    conn, addr = sock.accept()
+    print('connected:', addr)
 
-conn.close()
+    """цикл передачи данных"""
+    while True:
+        data = conn.recv(4096)
+        print(colored("data from home server", 'green', attrs=['reverse', 'blink']))
+        print(data)
+        if not data:
+            break
+        """обратная отправка данных"""
+        conn.send(data)
+
+    """Закрываем подключение"""
+    conn.close()
+
+i = 0
+for i in range(7):
+    i = i + 1
+    receive_data_from_server()
