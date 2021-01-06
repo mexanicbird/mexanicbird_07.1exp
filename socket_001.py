ed6f406b69d51eed81e37d7e89297864714945f4
Алексей Куликов <alex@MacBook-Pro-Aleksej.local>
2021-01-06 12:42:20 +0300

    commit_005_06.01.2021

--- socket_001.py
+++ socket_001.py
@@ -42,18 +42,20 @@ def receive_data_from_server():
     while True:
         """Запрос точного времени"""
         time_now = datetime.datetime.now()
-        data = conn.recv(4096)
+        try:
+            data = conn.recv(4096)
+        except:
+            break
         print(colored("data from home server", 'green', attrs=['reverse', 'blink']))
-        print(colored(str(time_now), 'green', attrs=['reverse', 'blink']))
-        print(data)
-        print(type(data))
+        print(colored(str(time_now) + '\n', 'green', attrs=['reverse', 'blink']))
+        #print(data)
+        #print(type(data))
 
         """обрабатываем данные"""
         data_str = data.decode('utf-8')
         print(colored(data_str, 'green', ))
         data_str_num = len(data_str)
         print(colored("Длина массива: " + str(data_str_num), 'blue'))
-
 
         """начало строк по меткам"""
         try:
@@ -108,34 +110,74 @@ def receive_data_from_server():
         print(colored("iii: " + str(num_data_i), 'blue'))
         print(colored("kkk: " + str(num_data_k), 'blue'))
         print(colored("lll: " + str(num_data_l), 'blue'))
-
-
+        print('\n')
 
         """перебор массива"""
         for i in data_str:
-            print(i)
             if i == 'a':
-                for i in data_str:
+                for i in data_str[num_data_a:data_str_num]:
                    if i == '\n':
                        break
                    else:
                        res_a_str.append(i)
-
-        for i in data_str:
-            print(i)
             if i == 'f':
-                print('ffff')
-                for i in data_str:
+                for i in data_str[num_data_f:data_str_num]:
                     if i == '\n':
-                        print('cccc')
                         break
                     else:
-                        print('eeee')
                         res_f_str.append(i)
+            if i == 'c':
+                for i in data_str[num_data_c:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_c_str.append(i)
+            if i == 'd':
+                for i in data_str[num_data_d:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_d_str.append(i)
+            if i == 'e':
+                for i in data_str[num_data_e:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_e_str.append(i)
+            if i == 'g':
+                for i in data_str[num_data_g:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_g_str.append(i)
+            if i == 'h':
+                for i in data_str[num_data_h:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_h_str.append(i)
+            if i == 'i':
+                for i in data_str[num_data_i:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_i_str.append(i)
+            if i == 'k':
+                for i in data_str[num_data_k:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_k_str.append(i)
+            if i == 'l':
+                for i in data_str[num_data_l:data_str_num]:
+                    if i == '\n':
+                        break
+                    else:
+                        res_l_str.append(i)
 
-
-
-
+            """Закрываем подключение"""
+            if i == 'q':
+                conn.close()
 
         print(colored("Полученные данные: ", 'yellow', attrs=['reverse', 'blink']))
         """обработка и печать выбранных строк"""
@@ -143,33 +185,89 @@ def receive_data_from_server():
         """обнуляем массив"""
         try:
             del res_a_str[0]
-            #res_a_str.pop(0)
+            del res_f_str[0]
+            del res_c_str[0]
+            del res_d_str[0]
+            del res_e_str[0]
+            del res_g_str[0]
+            del res_h_str[0]
+            del res_i_str[0]
+            del res_k_str[0]
+            del res_l_str[0]
         except:
             break
+
+        """собираем в одну строку из списка"""
         res_a_str_b = ''.join(res_a_str)
+        res_f_str_b = ''.join(res_f_str)
+        res_c_str_b = ''.join(res_c_str)
+        res_d_str_b = ''.join(res_d_str)
+        res_e_str_b = ''.join(res_e_str)
+        res_g_str_b = ''.join(res_g_str)
+        res_h_str_b = ''.join(res_h_str)
+        res_i_str_b = ''.join(res_i_str)
+        res_k_str_b = ''.join(res_k_str)
+        res_l_str_b = ''.join(res_l_str)
+
 
         """переводим во флоат"""
         try:
             res_a_float = float(res_a_str_b)
+            res_f_float = float(res_f_str_b)
+            res_c_float = float(res_c_str_b)
+            res_d_float = float(res_d_str_b)
+            res_e_float = float(res_e_str_b)
+            res_g_float = float(res_g_str_b)
+            res_h_float = float(res_h_str_b)
+            res_i_float = float(res_i_str_b)
+            res_k_float = float(res_k_str_b)
+            res_l_float = float(res_l_str_b)
         except:
             res_a_float = None
-        print(colored(res_a_str, 'cyan', ))
-        print(colored(res_a_str_b, 'cyan', ))
+            res_f_float = None
+            res_c_float = None
+            res_d_float = None
+            res_e_float = None
+            res_g_float = None
+            res_h_float = None
+            res_i_float = None
+            res_k_float = None
+            res_l_float = None
 
-
+        """печать переменных в строках"""
+        print(colored(res_a_str, 'green', ))
         print(colored(res_f_str, 'green', ))
+        print(colored(res_c_str, 'green', ))
+        print(colored(res_d_str, 'green', ))
+        print(colored(res_e_str, 'green', ))
+        print(colored(res_g_str, 'green', ))
+        print(colored(res_h_str, 'green', ))
+        print(colored(res_i_str, 'green', ))
+        print(colored(res_k_str, 'green', ))
+        print(colored(res_l_str, 'green', ))
 
-
-
-        print(colored(res_c_str, 'cyan', ))
-
-        """печать переменных"""
+        print('\n')
+        """печать переменных флоат"""
         print(colored(res_a_float, 'magenta'))
         res_a_str.clear()
         print(colored(res_f_float, 'magenta'))
         res_a_str.clear()
         print(colored(res_c_float, 'magenta'))
         res_a_str.clear()
+        print(colored(res_d_float, 'magenta'))
+        res_d_str.clear()
+        print(colored(res_e_float, 'magenta'))
+        res_e_str.clear()
+        print(colored(res_g_float, 'magenta'))
+        res_g_str.clear()
+        print(colored(res_h_float, 'magenta'))
+        res_h_str.clear()
+        print(colored(res_i_float, 'magenta'))
+        res_i_str.clear()
+        print(colored(res_k_float, 'magenta'))
+        res_k_str.clear()
+        print(colored(res_l_float, 'magenta'))
+        res_l_str.clear()
         print('')
         """если нет данных выход из цикла"""
         if not data:
@@ -179,7 +277,7 @@ def receive_data_from_server():
         #conn.send(data)
 
     """Закрываем подключение"""
-    conn.close()
+    #conn.close()
 
 #j = 0
 #for j in range(7):
